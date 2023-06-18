99ebf056503bc45f0f5265219e04036c69ebbbda
Nrmewtry <112067383+Nrmewtry@users.noreply.github.com>
2023-06-18 11:27:43 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -9,16 +9,17 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
-        await query.message.edit_text(
-            text = f"╭─《 🔰𝔸𝔹𝕆𝕌𝕋 𝕄𝔼🔰 》\n├  CREATOR: <a href='tg://user?id={OWNER_ID}'>🅷🅸🆃🅻🅴🆁</a>\n├  CHANNEL: <a href='https://t.me/Kan_Serial'>Serial Adda</a>\n├ LANGUAGE USED: Python\n╰  𝐓𝐇𝐀𝐍𝐊 𝐘𝐎𝐔❤️",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
+        await query.message.edit_media(
+            media=InputMediaPhoto("https://telegra.ph/file/da2484a78759e5c28ac90.jpg"),
+            caption=f"╭─《 🔰𝔸𝔹𝕆𝕌𝕋 𝕄𝔼🔰 》\n├  CREATOR: <a href='tg://user?id={OWNER_ID}'>🅷🅸🆃🅻🅴🆁</a>\n├  CHANNEL: <a href='https://t.me/Kan_Serial'>Serial Adda</a>\n├ LANGUAGE USED: Python\n╰  𝐓𝐇𝐀𝐍𝐊 𝐘𝐎𝐔❤️",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("🔒 Close", callback_data="close")
                     ],
                     [
-                        InlineKeyboardButton("⚡JOIN CHANNEL⚡", url = "https://t.me/+ozMvFa6su881YmM1")
+                        InlineKeyboardButton("⚡JOIN CHANNEL⚡", url="https://t.me/+ozMvFa6su881YmM1")
                     ]
                 ]
             )
@@ -29,3 +30,4 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             await query.message.reply_to_message.delete()
         except:
             pass
+
